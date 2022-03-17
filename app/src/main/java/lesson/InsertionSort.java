664f765ef383c0bbea836bef1078e52102c2c02d
shane panter <shane@foundationcode.com>
2022-03-17 13:31:29 -0600

    update interface

--- app/src/main/java/lesson/InsertionSort.java
+++ app/src/main/java/lesson/InsertionSort.java
@@ -9,8 +9,9 @@ public interface InsertionSort<T> {
      * Sorts a list in the order determined by the comparator
      * @param list The list to sort
      * @param compare The comparator
+     * @param print Print the contents of the list after each iteration
      */
-    public void sort(List<T> list, Comparator<T> compare );
+    public void sort(List<T> list, Comparator<T> compare, boolean print );
 
      /**
      * @return The number of Swaps that we made in the sort
